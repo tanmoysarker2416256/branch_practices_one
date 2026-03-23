158d9ccf53725305e279be53b65d987ccce4f69e
Tanmoy <tanmoysarker2416256@gmail.com>
2026-03-23 13:16:14 +0600

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace branch_practices_one.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

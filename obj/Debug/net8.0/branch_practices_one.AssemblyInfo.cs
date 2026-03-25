c91b4e06a1204af8dd8e319e1b14a84ef0580dff
Tanmoy <tanmoysarker2416256@gmail.com>
2026-03-25 21:43:04 +0600

    title change in dev branch

--- obj/Debug/net8.0/branch_practices_one.AssemblyInfo.cs
+++ obj/Debug/net8.0/branch_practices_one.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("branch_practices_one")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c57e062f5e4a71742d17d4eb10daefb4dddf0ab8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae12a5d1e1b13c5eaf1be82b30d5f61369eb75ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("branch_practices_one")]
 [assembly: System.Reflection.AssemblyTitleAttribute("branch_practices_one")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
